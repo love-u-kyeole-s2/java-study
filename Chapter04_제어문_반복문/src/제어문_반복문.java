4f4fb417a1383a5acaad3f1ab25286e1d003eae1
sist-3 <sist-3@sist3>
2026-03-10 14:25:57 +0900

    1111

---
+++ Chapter04_제어문_반복문/src/제어문_반복문.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ * 
+ * 	1. for =>126page
+ * 
+ * 		화면출력(UI)=>반복 횟수가 정해진 경우
+ * 	2. while=>131page
+ * 	3. do~while ==>134page
+ * 	-------------------
+ * 	반복제어
+ * 	1. break;
+ * 	2. continue;
+ * 
+ * 	최신 for-each 웹에서 주로 사용
+ * 		--------- 변경은 불가능=>only 출력==>배열/컬렉션
+ * 
+ * 
+ */
+public class 제어문_반복문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1~10까지 출력==> 반복횟수 지정=>for 
+		for(char i='A';i<='Z';i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
